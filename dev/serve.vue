95decec9ef64490b08c46c812bc0c95e9da80f6c
Jeff Rogers <jefrog1844@gmail.com>
2020-09-06 23:38:04 -0500

    add formatting

--- dev/serve.vue
+++ dev/serve.vue
@@ -4,32 +4,35 @@ import { MuiAppbar } from "@/entry";
 import { MuiPanel } from "@/entry";
 import { MuiButton } from "@/entry";
 import { MuiContainer } from "@/entry";
- 
+
 export default Vue.extend({
   name: "ServeDev",
   components: {
     MuiAppbar,
     MuiPanel,
     MuiButton,
-    MuiContainer
+    MuiContainer,
   },
 });
 </script>
 
 <template>
-
-    <mui-container fluid>
+  <mui-container fluid>
     <mui-panel>
-    <mui-appbar>
-      <table width="100%">
-        <tr style="vertical-align:middle;">
-          <td class="mui--appbar-height">Left Side</td>
-          <td class="mui--appbar-height" align="right">Right Side</td>
-        </tr>
-      </table>
-    </mui-appbar>
-    <mui-button>Click</mui-button><mui-button variant="flat" size="large" color="dark">Click2</mui-button>
+      <h1>Appbar</h1>
+      <mui-appbar>
+        <table width="100%">
+          <tr style="vertical-align:middle;">
+            <td class="mui--appbar-height">Left Side</td>
+            <td class="mui--appbar-height" align="right">Right Side</td>
+          </tr>
+        </table>
+      </mui-appbar>
     </mui-panel>
-    </mui-container>
-
+    <mui-panel>
+      <h1>Buttons</h1>
+      <mui-button>Click</mui-button>
+      <mui-button variant="flat" size="large" color="dark">Click2</mui-button>
+    </mui-panel>
+  </mui-container>
 </template>
