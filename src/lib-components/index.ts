2159a435dd2abd9817d8689fe50c1f567aa56a43
Jeff Rogers <jefrog1844@gmail.com>
2020-09-08 11:53:32 -0500

    add mui-row

--- src/lib-components/index.ts
+++ src/lib-components/index.ts
@@ -3,3 +3,4 @@ export { default as MuiAppbar } from './mui-appbar.vue';
 export { default as MuiPanel } from './mui-panel.vue';
 export { default as MuiButton } from './mui-button.vue';
 export { default as MuiContainer } from './mui-container.vue';
+export { default as MuiRow } from './mui-row.vue';
---
+++ src/lib-components/mui-row.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mui-row">
+    <slot></slot>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+@Component
+export default class MuiRow extends Vue {
+}
+</script>
